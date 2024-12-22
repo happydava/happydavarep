e88c040581f5282c648a0552e22eb792b43c5177
david_khamitskiy <davidxqd58@gmail.com>
2024-12-23 02:55:53 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+    Person person = new Person("David", "Khamitskiy", 18, true );
+    System.out.println(person.toString());
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+    public Person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String genderString = gender ? "Male" : "Female";
+        return "I'm " + name + " " + surname + ", " + age + " years old. Gender is " + genderString + ".";
+    }
+}
